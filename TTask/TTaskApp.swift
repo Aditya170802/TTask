64fbde07b14bd874205b027fa2e42535d75d3d6a
Aditya Ghuraiya <ghuraiyaaditya@gmail.com>
2025-03-05 21:15:53 +0530

    Initial Commit

---
+++ TTask/TTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTaskApp.swift
+//  TTask
+//
+//  Created by Aditya Ghuraiya on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
